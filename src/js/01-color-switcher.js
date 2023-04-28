10ffdbd24c949d17ccc52df99f895c3fd9e18a1e
YazikovXIII <vladzaraza@gmail.com>
2023-04-28 15:51:37 +0300

    123

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
+start.addEventListener('click', onStart);
+stop.addEventListener('click', onStop);
+
+let timerId = null;
+
+function onStart() {
+  start.disabled = true;
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function onStop() {
+  start.disabled = false;
+  clearInterval(timerId);
+//   document.body.style.backgroundColor = 'white';
+}
